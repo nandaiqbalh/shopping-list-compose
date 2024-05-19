1e2aa42191cfa21d47980503b1bf898b961b38c6
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2024-05-19 09:30:38 +0700

    First init

---
+++ app/src/main/java/com/nandaiqbalh/shoppinglist/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nandaiqbalh.shoppinglist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nandaiqbalh.shoppinglist.ui.theme.ShoppingListTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			ShoppingListTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					shoppingList()
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun shoppingList(){
+	Column (modifier = Modifier.fillMaxSize()) {
+		Button(onClick = { /*TODO*/ }) {
+			Text(text = "Add Item")
+		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun shoppingListPreview(){
+	shoppingList()
+}
